e210c3375b18d2d6e52fae702def0e85c01905d0
mksmvnv <mksmvnv@icloud.com>
2023-12-16 15:31:56 +0300

    Updated interactive mode

--- user_goals.py
+++ user_goals.py
@@ -1,15 +1,15 @@
 def user_goals():
     print("Welcome to your Calorie Tracker!")
-    GENDER = input("""
+    user_gender = input("""
 Choose your gender:
 1 - Male
 2 - Female
 3 - Other
 Enter number: """)
-    AGE = int(input("Age: "))
-    HEIGHT = float(input("Height: "))
-    WEIGHT = float(input("Weight: "))
-    ACTIVE_LEVEL = input("""
+    user_age = int(input("Age: "))
+    user_height = float(input("Height: "))
+    user_weight = float(input("Weight: "))
+    user_active_level = input("""
 Choose your activity level:
 1 - Sedentary lifestyle
 2 - Moderate activity
@@ -17,11 +17,13 @@ Choose your activity level:
 4 - Very active lifestyle
 Enter number: """)
 
-    match GENDER:
+    match user_gender:
         case "1":
-            BMR = 66 + (13.75 * WEIGHT) + (5 * HEIGHT) - (6.75 * AGE)
+            BMR = 66 + (13.75 * user_weight) + (5 * user_height) - (6.75 *
+                                                                    user_age)
         case "2":
-            BMR = 655 + (9.56 * WEIGHT) + (1.85 * HEIGHT) - (4.68 * AGE)
+            BMR = 655 + (9.56 * user_weight) + (1.85 * user_height) - (
+                4.68 * user_age)
         case "3":
             print("Sorry. On planet Earth we only have 2 genders.\n")
             exit()
@@ -29,7 +31,7 @@ Enter number: """)
             print("Invalid choice.")
             exit()
 
-    match ACTIVE_LEVEL:
+    match user_active_level:
         case "1":
             CALORIE_GOAL_LIMIT = BMR * 1.2
         case "2":
@@ -46,11 +48,11 @@ Enter number: """)
     FATS_GOAL = (CALORIE_GOAL_LIMIT * 0.3) / 9
     CARBS_GOAL = (CALORIE_GOAL_LIMIT * 0.4) / 4
 
-    goals_data = {
+    user_goals_data = {
         "CALORIE_GOAL_LIMIT": CALORIE_GOAL_LIMIT,
         "PROTEINS_GOAL": PROTEINS_GOAL,
         "FATS_GOAL": FATS_GOAL,
         "CARBS_GOAL": CARBS_GOAL
     }
 
-    return goals_data
+    return user_goals_data
